d7df6d80f7eeb1e02551baa65f1bb4d366156dbb
Alex Vong <avong003@gmail.com>
2021-09-30 14:11:31 -0700

    call to action

--- modules/call-to-action.php
+++ modules/call-to-action.php
@@ -5,8 +5,9 @@
 
 	<p class='calm-voice'>These aren't names you should memorize. We're just making them up. Sometimes things get called something so many times, that it sticks. This is a pretty common pattern</p>
 
+
 	<a href='#'>
-		<span>Here's the action!</span>
+		<span class='calm-voice'>Here's the action!</span>
 	</a>
 
 </call-to-action>
